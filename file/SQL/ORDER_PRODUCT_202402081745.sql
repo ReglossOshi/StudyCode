c71ab71e813efac98050386706154678bc0a9495
Regloss <bjp99112244@gmail.com>
2024-02-08 17:51:05 +0900

    수정

---
+++ file/SQL/ORDER_PRODUCT_202402081745.sql
@@ -0,0 +1,10 @@
+INSERT INTO SEMI_PROJECT2.ORDER_PRODUCT (ORDER_NUM,ORDER_DATE,REQUESTER,BRAND_NAME,ITEM_TYPE,ORDER_PLACE,ITEM_NAME,ITEM_SIZE,QUANTITY,CHEST,FRONT_LENGTH,SLEEVE_LENGTH,BACK_WIDTH,ARM_WIDTH,REQUIREMENTS,PRODUCT_DETAILS,FILE_PATH,STATUS) VALUES
+	 (45,TIMESTAMP'2024-02-01 09:31:26','김아무개','나이키','longsleeves','미국','스왓더','xs',100,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'done'),
+	 (46,TIMESTAMP'2024-02-01 09:34:21','매니저','나이키','hoodie','중국','스왓더','l',11,1,1,1,1,1,NULL,NULL,NULL,'진행중'),
+	 (47,TIMESTAMP'2024-02-01 09:36:03','매니저','나이키','polo','중국','스왓더','s',10,1,1,1,1,1,'test','test',NULL,'중지'),
+	 (43,TIMESTAMP'2024-01-31 19:05:34','홍길동','나이키','긴팔','중국','칠 니트','M',100,50,67,61,39,18,'스포츠웨어','61% 폴리에스터, 30% 라이오셀, 9% 스판덱스',NULL,'done'),
+	 (48,TIMESTAMP'2024-02-01 11:44:26','김사랑','나이키','longsleeves','미국','매직핏','s',23,1,1,1,1,1,'test',NULL,NULL,'진행중'),
+	 (81,TIMESTAMP'2024-02-03 15:36:23','이현주','나이키','후디','중국','피닉스 플리스','s',10,81,81,0,0,0,'핑크색
+','몸체: 80% 면/20% 폴리에스터. 후드 안감: 100% 면.','niki1.PNG','진행중'),
+	 (61,TIMESTAMP'2024-02-01 18:11:43','김다은','나이키','긴팔','방글라데시','ACG 써마 핏','M',5,90,0,0,0,0,'가슴둘레 엉덩이둘래 90cm
+블랙/서밋 화이트','75% 면/25% 폴리에스터. 골지: 98% 면/2% 스판덱스','긴팔.PNG','완료');
---
+++ file/SQL/PRODUCTS_202402081745.sql
@@ -0,0 +1,24 @@
+INSERT INTO SEMI_PROJECT2.PRODUCTS (ITEM_NUM,GENDER,CATEGORY,ITEM_TYPE,ITEM_NAME,ITEM_SIZE,COLOR,PRICE) VALUES
+	 (456,'W','후디&크루','폴로','김다은 세번째','X','pink',555),
+	 (66,'M','후디&크루','반팔','페이징2','L','balck',123),
+	 (2,'W','탑&티셔츠','긴팔','칠 니트','M','pink',65000),
+	 (3,'W','탑&티셔츠','긴팔','칠 니트','L','blue',65000),
+	 (4,'W','후디&크루','후디','스포츠웨어 클럽','XS','black',95000),
+	 (5,'W','후디&크루','후디','스포츠웨어 클럽','S','dark grey',95000),
+	 (6,'M','탑&티셔츠','반팔','나이키 드라이 핏','M','white',49000),
+	 (7,'M','탑&티셔츠','반팔','나이키 드라이 핏','L','black',49000),
+	 (8,'M','탑&티셔츠','폴로','나이키 클럽','M','white',50100),
+	 (1,'W','탑&티셔츠','긴팔','칠 니트','S','black',65000);
+INSERT INTO SEMI_PROJECT2.PRODUCTS (ITEM_NUM,GENDER,CATEGORY,ITEM_TYPE,ITEM_NAME,ITEM_SIZE,COLOR,PRICE) VALUES
+	 (9,'M','탑&티셔츠','폴로','나이키 클럽','L','black',50100),
+	 (16,'W','후디&크루','반팔','김다은두번째','XL','pink',555),
+	 (11,'M','탑&티셔츠','폴로','실험','XL','green',50100),
+	 (12,'M','탑&티셔츠','폴로','Test','XL','green',50100),
+	 (98,'M','탑&티셔츠','폴로','Test','XL','green',50100),
+	 (165,'M','탑&티셔츠','폴로','Test','XL','green',50100),
+	 (18,'M','탑&티셔츠','폴로','Test','XL','green',50100),
+	 (118,'M','탑&티셔츠','폴로','Test','XL','green',50100),
+	 (52,'M','탑&티셔츠','폴로','Test','XL','green',50100),
+	 (62,'M','탑&티셔츠','폴로','Test','XL','green',50100);
+INSERT INTO SEMI_PROJECT2.PRODUCTS (ITEM_NUM,GENDER,CATEGORY,ITEM_TYPE,ITEM_NAME,ITEM_SIZE,COLOR,PRICE) VALUES
+	 (10,'M','탑&티셔츠','폴로','나이키 클럽','XL','green',50100);
