0c6212f3f2eaea6840fc7c879b13658c19b854d6
Regloss <bjp99112244@gmail.com>
2024-02-08 17:22:20 +0900

    삭제

--- Example/java_web_sol/project/airbnb/index.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.category-button').forEach(button => {
-  button.addEventListener("click", event => {
-    document.querySelector('#active-category-button').removeAttribute('id');
-    button.setAttribute('id', 'active-category-button');
-  })
-});
-
