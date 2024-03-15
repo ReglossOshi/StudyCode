2051e4f10dda93801b0e0a5f8887717c39c96c3b
Regloss <bjp99112244@gmail.com>
2024-03-15 16:23:50 +0900

    수정

--- nike/src/main/java/com/projectnike/nike/mapper/StoreMapper.java
+++
@@ -1,9 +0,0 @@
-package com.projectnike.nike.mapper;
-
-import com.projectnike.nike.domain.ProductsVO;
-
-import java.util.List;
-
-public interface StoreMapper {
-    public List<ProductsVO> searchItems(String search);
-}
--- nike/src/main/java/com/projectnike/nike/service/StoreService.java
+++
@@ -1,9 +0,0 @@
-package com.projectnike.nike.service;
-
-import com.projectnike.nike.domain.ProductsVO;
-
-import java.util.List;
-
-public interface StoreService {
-    public List<ProductsVO> getList(String search);
-}
