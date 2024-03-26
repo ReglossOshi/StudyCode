b547694a606df4439dfb22ba77d8038195e781d9
Regloss <bjp99112244@gmail.com>
2024-03-26 18:53:53 +0900

    수정

---
+++ Spring/ex02/src/main/java/org/zerock/service/MemberServiceImpl.java
@@ -0,0 +1,27 @@
+package org.zerock.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.zerock.domain.MemberVO;
+import org.zerock.mapper.MemberMapper;
+
+@Service
+public class MemberServiceImpl implements MemberService {
+    @Autowired
+    private MemberMapper memberMapper;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public int putMember(MemberVO memberVO){
+        memberVO.setRole("ROLE_USER");
+        String rawPwd = memberVO.getPassword();
+        String encryptPwd = bCryptPasswordEncoder.encode(rawPwd);   // 평문을 암호화
+        memberVO.setPassword(encryptPwd);
+
+        return memberMapper.insertMember(memberVO);
+    }
+
+}
